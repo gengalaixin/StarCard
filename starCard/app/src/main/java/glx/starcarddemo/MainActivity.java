8cf95f4c34c2a6851c4bec94223921a7cfdd8c19
耿来新 <1099344470@qq.com>
2017-09-25 16:33:28 +0800

    no message

---
+++ starCard/app/src/main/java/glx/starcarddemo/MainActivity.java
@@ -0,0 +1,13 @@
+package glx.starcarddemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
